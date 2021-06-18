452ff5f2d26829091c65fe5504d0753fee6e79c9
Raushan1123 <raushan00567@gmail.com>
2021-06-19 00:21:11 +0530

    http get and data parsing

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
-import '../change_name_card.dart';
+import 'package:http/http.dart' as http;
 import '../drawer.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,37 +11,64 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController _nameController = TextEditingController();
   var myText = "Change me";
+  var url  = "https://jsonplaceholder.typicode.com/photos";
+  var data;
+
   @override
   void initState(){
     super.initState();
-  }
+    getData();
+      }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-     body: Padding(
-       padding: const EdgeInsets.all(16.0),
-       child: SingleChildScrollView(
-                child: Card(
-           child: ChangeNameCard(myText: myText, nameController: _nameController),
+     getData() async {
+       var res = await http.get(Uri.parse(url));
+       //print(res.body);
+       data = jsonDecode(res.body);
+       setState(() {
+         
+       });
+     }
+    
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          backgroundColor: Colors.grey[200],
+          appBar: AppBar(
+            title: Text("Awesome App"),
+          ),
+         body: Padding(
+           padding: const EdgeInsets.all(16.0),
+           child: data!=null?
+           ListView.builder(
+             itemBuilder: (context,index){
+               return Padding(
+                 padding: const EdgeInsets.all(16.0),
+                 child: ListTile(
+                   title: Text(data[index]["title"]),
+                   subtitle: Text("ID: ${data[index]["id"]}"),
+                   leading: Image.network(data[index]["url"]),
+                 ),
+               );
+             },
+             itemCount: data.length,
+             )
+           :Center(
+             child: CircularProgressIndicator(),
+           ),
          ),
-       ),
-     ),
-     drawer: MyDrawer(),
-       floatingActionButton: FloatingActionButton(
-         onPressed: () {
-           myText = _nameController.text;
-           setState(() {
-             
-           });
-         },
-         child: Icon(Icons.refresh),
-       ),
-      );
-  }
+         drawer: MyDrawer(),
+           floatingActionButton: FloatingActionButton(
+             onPressed: () {
+               myText = _nameController.text;
+               setState(() {
+                 
+               });
+             },
+             child: Icon(Icons.refresh),
+           ),
+          );
+      }
+    
+    
 }
 
