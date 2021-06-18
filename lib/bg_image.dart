5aded8a5b72a084d53f0f89e48d511346c7273a9
Raushan1123 <raushan00567@gmail.com>
2021-06-19 00:38:25 +0530

    Forms,Stacks & Navigation

--- lib/bg_image.dart
+++ lib/bg_image.dart
@@ -5,7 +5,9 @@ class BgImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
      return  Image.asset("assets/desola-lanre-ologun-zYgV-NGZtlA-unsplash.jpg",
-               fit: BoxFit.cover,
+              fit: BoxFit.cover,
+              color: Colors.black.withOpacity(0.7),
+              colorBlendMode: BlendMode.darken,
                );
   }
 }
